cba0e656c61c6037afb837dac913fd6ccd76e133
ahmed boutayeb <instinctauto@gmail.com>
2021-06-30 15:30:46 +0100

    dockerizing the backend of basma 3

--- backend/src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
+++ backend/src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
@@ -2,9 +2,12 @@ package com.luv2code.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class SpringBootEcommerceApplication {
+@ComponentScan("com.luv2code")
+public class SpringBootEcommerceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEcommerceApplication.class, args);
